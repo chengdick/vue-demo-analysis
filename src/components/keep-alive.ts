35f56c0ed78b3fc6c6a4998fbbffdec305ee20e7
chengshuping <chengshuping9@163.com>
2021-04-08 17:26:14 +0800

    优化

--- src/components/keep-alive.ts
+++ src/components/keep-alive.ts
@@ -1,11 +1,10 @@
 class LRUCache {
   capacity: number;
   keys: number[] = [];
-  cache: Record<string, any> = {};
+  cache: Record<string, any> = Object.create(null);
   constructor(capacity: number) {
     this.capacity = capacity;
   }
-
   get(key: number) {
     if (this.keys.includes(key)) {
       const index = this.keys.findIndex((item: number) => item === key);
@@ -14,11 +13,10 @@ class LRUCache {
     }
     return this.cache[key] || -1;
   }
-
   put(key: number, value: any) {
     this.cache[key] = value;
     if (this.keys.includes(key)) {
-      let index = this.keys.findIndex((item: number) => item === key);
+      const index = this.keys.findIndex((item: number) => item === key);
       this.keys.splice(index, 1);
     }
     this.keys.unshift(key);
@@ -36,7 +34,7 @@ lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
 lRUCache.get(1); // 返回 1
 lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
 lRUCache.get(2); // 返回 -1 (未找到)
-lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}+++++++++++++++++++++++
+lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
 lRUCache.get(1); // 返回 -1 (未找到)
 lRUCache.get(3); // 返回 3
 lRUCache.get(4); // 返回 4
